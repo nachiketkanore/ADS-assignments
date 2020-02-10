36cf084729971207739a80f39255c9a7f079b3bd
nachiketkanore <44920607+nachiketkanore@users.noreply.github.com>
2020-02-10 10:13:19 +0530

    Update Kruskal.cpp

--- Kruskal.cpp
+++ Kruskal.cpp
@@ -1,5 +1,5 @@
 //============================================================================
-// Name        : cpp.cpp
+// Name        : 
 // Author      : Nachiket Kanore
 // Version     :
 // Copyright   : Your copyright notice
@@ -7,9 +7,11 @@
 //============================================================================
 
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
-const int N = 100;
+const int N = 50;
+const int inf = 1e5;
 
 struct edge{
 	int from , to , w;
@@ -21,7 +23,8 @@ class graph{
 		int nodes , edges;
 		string cityIndex[N];
 		edge edgeList[N];
-		int spanning[N];
+		edge spanning[N];
+		int result[N][N];
 		int sz = 0;
 	public:
 		graph(){
@@ -30,22 +33,27 @@ class graph{
 				for(int j = 0; j < N; j++)
 					mat[i][j] = 0;
 		}
-		
-		void createComponents(){
-			
-		}
 
 
 		void printMat(){
 			cout << "Adjacency Matrix of given network :" << endl;
 			for(int i = 1; i <= nodes; i++){
-				cout << cityIndex[i] << " => ";
 				for(int j = 1; j <= nodes; j++)
-					cout << mat[i][j] << " ";
+					cout << setw(2) << left << mat[i][j] << " ";
 				cout << endl;
 			}
 
 		}
+
+		void printMat2(){
+			cout << "Adjacency Matrix of spanning tree :" << endl;
+			for(int i = 1; i <= nodes; i++){
+				for(int j = 1; j <= nodes; j++)
+					cout << setw(2) << left << result[i][j] << " ";
+				cout << endl;
+			}
+		}
+
 		int Index(string s){
 			for(int i = 1; i <= nodes; i++)
 				if(cityIndex[i] == s)
@@ -56,38 +64,73 @@ class graph{
 			cout << "Enter the number of cities :";	cin >> nodes;
 			cout << "Enter the number of connections :";	cin >> edges;
 
-			for(int i = 1; i <= nodes; i++){
-				cout << "Enter the city number " << i << " : ";
-				cin >> cityIndex[i];
-			}
 
-			cout << "Enter the edges of network " << endl;
+
+			cout << "Enter the edges of network: " << endl;
+			cout << "{From , to , weight}" << endl;
 			int e = edges;
 			for(int e = 1; e <= edges; e++){
-				string from , to;	int w;
-				cout << "From :";	cin >> from;
-				cout << "To :";	cin >> to;
-				cout << "Weight :";	cin >> w;
-				int ind1 = Index(from) , ind2 = Index(to);
+				int from , to;	int w;
+				cin >> from;	cin >> to;	cin >> w;
+				int ind1 = from , ind2 = to;
 				mat[ind1][ind2] = w , mat[ind2][ind1] = w;
 				edgeList[e].from = ind1 , edgeList[e].to = ind2 , edgeList[e].w = w;
 			}
 		}
 
-		void MST(){
-			for(int i = 1; i <= edges; i++)
-				for(int j = i+1; j <= edges; j++)
-					if(edgeList[i].w > edgeList[j].w)
-						swap(edgeList[i] , edgeList[j]);
+
+
+		void MST_PRIMS(){
 
 			int weight = 0;
+			int taken[N] = {0};
+			taken[1] = 1;
+			int cost = 0;
 
-			for(int i = 1; i <= edges; i++){
-				edge curr = edgeList[i];
-				if(sameComponent(curr.from , curr.to))	continue;
-				weight += curr.w;
-				unite(curr.from , curr.to);
+			int ind = 1;
+
+			int done = 1;
+			while(done != nodes){
+				int from  = 0 , to = 0 , w = inf;
+				for(int i = 1; i <= nodes; i++){
+					if(taken[i]){
+
+						for(int j = 1; j <= nodes; j++){
+							int ww = mat[i][j];
+							if(ww == 0) continue;
+								if(!taken[j] and w > ww)
+									from = i , to = j , w = ww;
+
+						}
+
+					}
+				}
+
+				if(w != inf){
+					cost += w;
+					taken[to] = 1;
+					edge add;
+					add.from = from , add.to = to, add.w = w;
+					done++;
+					spanning[ind++] = add;
+				}
+
 			}
+
+			cout << "Spanning tree of given graph :" << endl;
+			cout << "Cost = " << cost << endl;
+			for(int i = 1; i <= nodes-1; i++){
+				result[spanning[i].from][spanning[i].to] = spanning[i].w;
+				result[spanning[i].to][spanning[i].from] = spanning[i].w;
+
+				cout << spanning[i].from << " " << spanning[i].to << " ";
+				cout << spanning[i].w << endl;
+			}
+				cout << "=================" << endl;
+				cout << "=================" << endl;
+				cout << "Adjacency matrix of spanning tree :" << endl;
+
+
 
 		}
 
@@ -98,15 +141,46 @@ int main() {
 
 	graph g;
 	g.init();
-	g.createComponents();
-	g.MST();
+	g.MST_PRIMS();
+
+
 
 	return 0;
 }
 
 
 
+/*
+ 1 2 10
+1 3 11
+2 3 12
+2 4 13
+3 5 16
+3 6 14
+4 5 17
+5 6 15
+Answer:
+Spanning tree of given graph :
+Cost = 63
+1 2 10
+1 3 11
+2 4 13
+3 6 14
+6 5 15
+--------
+1 2 10
+2 3 20
+3 5 30
+4 5 1
+1 4 5
+Spanning tree of given graph :
+Cost = 36
+1 4 5
+4 5 1
+1 2 10
+2 3 20
 
+ */
 
 
 
